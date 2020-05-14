8f5a79ea041b46c3d3e7cd6acfa0c8f5f80e70a8
Kunal Raj Bhardwaj <kunalraj1705@gmail.com>
2020-05-14 13:43:19 +0530

    Added : BinaryNumber code

---
+++ src/IntegerProb/BinaryNumber.java
@@ -0,0 +1,33 @@
+package IntegerProb;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BinaryNumber {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        //Scanning the Integer number
+        System.out.println("Enter the number you want to convert to binary : ");
+        int n = sc.nextInt();
+
+        //Converting the Integer to Binary
+        String binaryString = Integer.toString(n, 2);
+        System.out.println("Binary Number : "+binaryString);
+        String binaryStringnew = Integer.toBinaryString(n);
+        System.out.println("New method for converting to binary : "+binaryStringnew);
+
+        //Converting the String to array separated by 0
+        String [] binaryArray = binaryString.split("0");
+
+        //Maximum number of consecutive 1 in binary
+        int l = 0;
+        for(int i = 0; i < binaryArray.length; i++ ){
+            if(l < binaryArray[i].length()){
+                l = binaryArray[i].length();
+            }
+        }
+        System.out.println("Binary array of consecutive 1 : "+ Arrays.toString(binaryArray));
+        System.out.println("Maximum number of consecutive 1 in the Binary is : "+l);
+        sc.close();
+    }
+}
