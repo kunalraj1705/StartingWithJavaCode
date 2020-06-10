709cf8d0ab26d39738fdcce309b5c536aa15aa68
Kunal Raj Bhardwaj <kunalraj1705@gmail.com>
2020-06-10 12:05:04 +0530

    Added : Number Formatter

---
+++ src/dataStructure/Numbers/NumberFormatter.java
@@ -0,0 +1,27 @@
+package dataStructure.Numbers;
+
+import javax.swing.*;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFormatter {
+    public static void main(String [] args){
+        String inputNumber = JOptionPane.showInputDialog("Enter a Number for formatting");
+        NumberFormat formatter = NumberFormat.getNumberInstance();
+        formatter.setMaximumFractionDigits(3);
+        formatter.setMinimumIntegerDigits(6);
+
+        Double inNumber = Double.parseDouble(inputNumber);
+        inNumber = inNumber / 3.0;
+
+        String formattedNumber = formatter.format(inNumber);
+        JOptionPane.showMessageDialog(null,  "Formatted Number after diving it by 3.0 is"+formattedNumber);
+
+        formatter = NumberFormat.getCurrencyInstance(new Locale("en", "IN"));
+        formatter.setMinimumIntegerDigits(6);
+        formatter.setMaximumFractionDigits(2);
+        String currency = formatter.format(inNumber);
+        JOptionPane.showMessageDialog(null, "Currency format after dividing the number by 3.0 "+currency);
+
+    }
+}
